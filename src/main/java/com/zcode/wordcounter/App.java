9d68d79118f252b4688c23f307b69680cc789ac4
Yazan Kassouaa <yazan.kassouaa@gmail.com>
2025-07-10 22:43:45 +0200

    Initialize word counter application

---
+++ src/main/java/com/zcode/wordcounter/App.java
@@ -0,0 +1,10 @@
+package com.zcode.wordcounter;
+
+/**
+ * Hello Word Counter!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Word Counter!");
+    }
+}
