b6457692068614f56d529715477958a81f2498d3
Yazan Kassouaa <yazan.kassouaa@gmail.com>
2025-07-11 11:22:37 +0200

    Close textPrompt before application exits.

--- src/main/java/com/zcode/wordcounter/App.java
+++ src/main/java/com/zcode/wordcounter/App.java
@@ -6,11 +6,7 @@ public class App {
 
     public static void main(String[] args) {
         WordCounterManager wordCounterManager = new WordCounterManager();
-        try {
-            wordCounterManager.askUserForTextAndCountWords();
-        } finally {
-            wordCounterManager.exit();
-        }
+        wordCounterManager.askUserForTextAndCountWords();
     }
 
 }
